7060f591c48ca5e42eded379098997361ecae425
amineennaciri <spitlight.enn@gmail.com>
2023-07-24 14:23:23 +0100

    this is a tic-tac-toe game

--- react-cv-app/src/main.jsx
+++ react-cv-app/src/main.jsx
@@ -1,7 +1,14 @@
-import React from 'react'
+/* import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import App from './App.jsx' */
+/* import './index.css' */
+
+import React, { StrictMode } from "react";
+/* import { createRoot } from 'react-dom/client'; */
+import ReactDOM, { createRoot } from 'react-dom/client'
+import './styles.css';
+
+import App from './App.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
