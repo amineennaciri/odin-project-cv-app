31f477dbe9b535ec7fd957725f0c3f4b62ad3264
amineennaciri <spitlight.enn@gmail.com>
2023-07-27 11:42:47 +0100

    commit

--- react-cv-app/src/App.jsx
+++ react-cv-app/src/App.jsx
@@ -9,12 +9,12 @@ function Square({value, onSquareClick}) {
   return <button className="square" onClick={onSquareClick}>{value}</button>;
 }
 
-export default function Board(){
+function Board(){
   const [xIsNext, setXIsNext] = useState(true);
   const [squares, setSquares] = useState(Array(9).fill(null));
 
   function handleClick(i){
-    if(squares[i]) {
+    if(squares[i] || calculateWinner(squares)) {
       return;
     }
     const nextSquares = squares.slice();
@@ -27,8 +27,18 @@ export default function Board(){
     setXIsNext(!xIsNext);
   }
 
+  const winner = calculateWinner(squares);
+  let status;
+  if (winner){
+    status = "Winner: " + winner;
+  }else {
+    status = "Next player: " + (xIsNext?"X":"O");
+  }
+
+
   return (
   <>
+  <div className='status'>{status}</div>
   <div className="board-row">
       <Square value={squares[0]} onSquareClick={ () => handleClick(0)}/>
       <Square value={squares[1]} onSquareClick={ () => handleClick(1)}/>
@@ -48,6 +58,40 @@ export default function Board(){
   );
 }
 
+export default function Game(){
+  const [xIsNext, setXIsNext] = userState(true);
+  const [history, setHistory] = useState([Array(9).fill(null)]);
+  return (
+    <div className='game'>
+      <div className='game-board'>
+        <Board />
+      </div>
+      <div className='game-info'>
+        <ol>{/* TODO */}</ol>
+      </div>
+    </div>
+  );
+}
+
+function calculateWinner(squares){
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i=0;i< lines.length;i++){
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]){
+      return squares[a];
+    }
+  }
+  return null;
+}
 
 
 /* import { useState } from 'react'
